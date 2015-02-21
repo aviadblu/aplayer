db0f70b69e90d39bd3af51f5bafecae528c80130
Aviad <aviad@codefresh.io>
2015-02-21 19:20:28 +0200

    low per

--- Gruntfile.js
+++ Gruntfile.js
@@ -302,10 +302,10 @@ module.exports = function (grunt) {
         'compass'
       ],
       dist: [
-        'coffee',
+        //'coffee',
         //'compass:dist',
-        'imagemin',
-        'svgmin',
+        //'imagemin',
+        //'svgmin',
         'htmlmin'
       ]
     },
